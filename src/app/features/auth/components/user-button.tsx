b56a675fe3e09bbe482139ca38081916daa388bc
Dmytro Masalov <dmitriymasalov5@gmail.com>
2025-04-20 10:20:14 +0300

    Rename a folder for api calls

--- src/app/features/auth/components/user-button.tsx
+++ src/app/features/auth/components/user-button.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useAuthActions } from "@convex-dev/auth/react";
-import { useCurrentUser } from "../hooks/use-current-user";
+import { useCurrentUser } from "../api/use-current-user";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { Loader, LogOut } from "lucide-react";
