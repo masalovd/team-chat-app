a6fa7015879624c2f783608c4cf36c96c3cd9865
Dmytro Masalov <dmitriymasalov5@gmail.com>
2025-04-25 12:25:40 +0300

    Enhance loading state checks in WorkspaceIdPage to include current member loading

--- src/app/workspaces/[workspaceId]/page.tsx
+++ src/app/workspaces/[workspaceId]/page.tsx
@@ -46,7 +46,7 @@ const WorkspaceIdPage = () => {
     setOpen
   ])
 
-  if (isWorkspaceLoading || isChannelsLoading) {
+  if (isWorkspaceLoading || isChannelsLoading || isCurrentMemberLoading) {
     return (
       <div className="h-full flex-1 flex items-center justify-center flex-col gap-2">
         <Loader className="size-6 animate-spin text-muted-foreground" />
@@ -54,7 +54,7 @@ const WorkspaceIdPage = () => {
     );
   }
 
-  if (!workspace) {
+  if (!workspace || !currentMember) {
     return (
       <div className="h-full flex-1 flex items-center justify-center flex-col gap-2">
         <TriangleAlert className="size-6 text-muted-foreground" />
